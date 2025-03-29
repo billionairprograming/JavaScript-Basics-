de95c5a4e9d165cb4c96c481e5d80c1a8405e7a6
billionairprograming <jaquel.dwhitee@gmail.com>
2025-03-29 19:35:51 -0400

    'Data types'This is going over differnt js data types and how they look

---
+++ java-types/index.js
@@ -0,0 +1,17 @@
+// This is going over primative data types and relative data types
+
+//- primitive data types
+let num = 12345;
+let name = "Jay";
+let hasLife = false;
+
+//- reference data types
+let obj = { key: value };
+let arry = [values];
+
+//this is another reference data type
+function someFunc() {}
+
+// Primitive data types are data types - These are basic data types that store single values and are immutable (cannot be changed). When assigned to a variable, they are stored directly in memory.
+
+// Reference data types - These are more complex types that store objects rather than single values. Instead of storing data directly, variables hold a reference (memory address) pointing to the actual object.
