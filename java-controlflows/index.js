7c2b8cedec741e350386ee185fb8e026637424e9
billionairprograming <jaquel.dwhitee@gmail.com>
2025-03-31 07:05:24 -0400

    'Loops and control flows' Here we went over if statments, for loops and while loops

---
+++ java-controlflows/index.js
@@ -0,0 +1,93 @@
+let x = 24;
+let y = 51;
+
+let nameArry = ["jason", "John", "Terry", "Steven", "Bob"];
+
+// Control Flows
+
+// if statments
+// if (x == 25) {
+//   console.log("Your not a grown ass man");
+// }
+// the if statement is for if there is a condition for somthing for the rest of the code to run
+
+//if else statments
+// if (x !== 25) {
+//   console.log("Your almost a man");
+// } else {
+//   console.log("Your a man now ");
+// }
+
+// this is for when if you want somthing else to happen once the if statement is done running and you wnat something else to happen.
+
+// else if statments
+// if (x !== 25 || y !== 50) {
+//   console.log("Your old enough but not old enough");
+// } else if (x == 25 || y == 50) {
+//   console.log("Your there now");
+// }
+// This is if there are muipltle conditions that can be ran if one is not true.
+
+// Swicth Cases
+
+// switch (x) {
+//   case "22":
+//     console.log("Too Young");
+//     break;
+
+//   case 24:
+//     console.log("Your a bit older");
+//     break;
+
+//   case "25":
+//     console.log("You might of age now");
+//     break;
+
+//   default:
+//     console.log("Your good bro");
+// }
+
+// This is another way of writing an if else statment but you can have muiltpile cases into this.
+
+//For loops
+// for (let i = 0; i < nameArry.length; i++) {
+//   console.log(nameArry[i]);
+// }
+// The for loop is for iterating through an array to show what is in it. This can be a little bit tricky so make sure to understand this deeply
+
+//While Loops
+// while (x < 30) {
+//   console.log("You are", x);
+//   x++;
+// }
+// This loop will keep on running unless you stop it and it will keep running until the condtion is true.
+
+// Do while loops
+// do {
+//   console.log("x is", x);
+//   x++;
+// } while (x < 24);
+
+// this will run once no matter what but im not sure when this  would be a good case to use
+
+// breaks and continues
+
+// while (x < 35) {
+//   console.log("you are", x);
+//   x++;
+//   break;
+// }
+// Breaking breaks the loop once the conditions are met and will stop the loop
+
+// continue the loop
+
+// while (x < 30) {
+//   x++;
+//   if (x == 28) {
+//     console.log("Were done here", x);
+//     continue;
+//   }
+//   console.log(x);
+// }
+
+// contiueing a loop when another condition is met and the loop will keep going until the loop is compeleted.
