be904e63d0850059f0fb447ad6fa02a37b1ef37a
billionairprograming <jaquel.dwhitee@gmail.com>
2025-03-31 20:24:57 -0400

    'Functions' Learned about diffent types of functions and some higher order functions

---
+++ java-functions/index.js
@@ -0,0 +1,52 @@
+//Functions
+
+let arry = ["tom", "jerry", "spongebob", "patrick"];
+
+// Function Declerations
+// function sayHello() {
+//   console.log("Hello James");
+// }
+// sayHello();
+//this is a normal function that is needed to be called to be invoked
+
+// Functions with returns
+// function hiName(name) {
+//   return name;
+// }
+// let greetings = hiName("Greg");
+// console.log("hello", greetings);
+
+// using the return statement the value is sent back to the function and can only be seen when called
+
+// Function Expressions
+// const greeting = function () {
+//   console.log("Hi I am an function expression");
+// };
+
+// greeting();
+
+// this is a function expression this can be added to a const or a let some kind of expression
+
+//Arrow Functions
+
+// const whoAmI = () => {
+//   console.log("This is an arrow function");
+// };
+
+// whoAmI();
+
+// these work the same as a regular function but instead they just dont have the words "function"
+
+// Higher reduces fucntions( methods with arrow funcutions in them.)
+
+// map method
+
+// let name = arry.map((names) => names);
+// console.log(name);
+// This is a another form of a loop that will return another arry that doesnt affect the orignal.
+// these are best with objects depeneing on the use case.
+
+// let cartoonNetwork = arry.filter((names) => names == "tom");
+// console.log(cartoonNetwork);
+
+// this takes out what it is that you are wanting to filter out - something im  still working on.
